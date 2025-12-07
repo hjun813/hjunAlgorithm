8371e6bf17a2703921ae7e7f2a5a24ab00e061cf
hjun813 <hyeonjun0899@gmail.com>
2025-12-08 01:18:39 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+dp = [-1] * (n+1)
+
+def fibo(n):
+
+    if dp[n] == -1:
+        dp[n] = fibo(n-1) + fibo(n-2)
+    return dp[n]
+
+dp[0] = 0
+dp[1] = 1
+print(fibo(n))
