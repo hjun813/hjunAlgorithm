07b0cf0bc1bf37c7469336298a16c9b219e664dc
hjun813 <hyeonjun0899@gmail.com>
2025-12-08 01:30:50 +0900

    [Silver I] Title: 경로 찾기, Time: 124 ms, Memory: 111044 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+matrix = []
+for _ in range(N):
+    matrix.append(list(map(int, input().split())))
+
+
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            if matrix[i][k] == 1 and matrix[k][j] == 1:
+                matrix[i][j] = 1
+
+for i in range(N):
+    print(*matrix[i])
